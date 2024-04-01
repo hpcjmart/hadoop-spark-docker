b6736babc58e3d761b3bb1c51365f105a10fe8d1
Jose A. Martinez <jmartine@ual.es>
2024-04-01 13:03:28 +0200

    directorio history master y worker

---
+++ master/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z "$(ls -A "$NAMEDIR")" ]; then
+  echo "Formatting namenode name directory: $NAMEDIR"
+  hdfs namenode -format
+fi
+
+echo "Starting Hadoop name node..."
+hdfs --daemon start namenode
+
+#echo "Starting Hadoop secondary name node..."
+#hdfs --daemon start secondarynamenode
+
+echo "Starting Hadoop resource manager..."
+yarn --daemon start resourcemanager
+
+if ! hdfs dfs -test -d /tmp
+then
+  echo "Formatting directory: /tmp"
+  hdfs dfs -mkdir -p  /tmp
+fi
+if ! hdfs dfs -test -d "$SPARK_LOGS_HDFS_PATH"
+then
+  echo "Formatting directory: $SPARK_LOGS_HDFS_PATH"
+  hdfs dfs -mkdir -p  "$SPARK_LOGS_HDFS_PATH"
+fi
+
+echo "Starting Spark master node..."
+spark-class org.apache.spark.deploy.master.Master --ip "$SPARK_MASTER_HOST"
